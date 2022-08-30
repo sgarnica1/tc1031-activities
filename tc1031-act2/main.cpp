80063e75187d7a617e09b7823ddcfaa87807a6bd
Sergio Garnica <sgarnica1902@gmail.com>
2022-08-30 16:38:39 -0500

    Main completo

--- tc1031-act2/main.cpp
+++ tc1031-act2/main.cpp
@@ -1,12 +1,13 @@
 // =================================================================
 //
 // File: main.cpp
-// Author:
-// Date:
+// Author: Sergio Garnica González - A01704025
+// Date: 30/08/2022
 //
 // =================================================================
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include "header.h"
 #include "search.h"
 #include "bubble.h"
@@ -15,6 +16,64 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-	return 0;
+int main(int argc, char *argv[])
+{
+  ifstream inputFile;
+  ofstream outputFile;
+
+  if (argc != 3)
+  {
+    cout << "usage: " << argv[0] << " input_file output_file\n";
+    return -1;
+  }
+
+  inputFile.open(argv[1]);
+  outputFile.open(argv[2]);
+
+  if (!inputFile.is_open())
+  {
+    cout << argv[0] << ": File \"" << argv[1] << "\" not found\n";
+    return -1;
+  }
+
+  // Cantidad de elementos del vector
+  int n, q, num;
+  inputFile >> n;
+  vector<int> nums(n);
+
+  // Inicializar vector
+  for (int i = 0; i < n; i++)
+  {
+    inputFile >> num;
+    nums[i] = num;
+  }
+
+  vector<int> disordered_nums = nums;
+  int selection_c, insertion_c, bubble_c;
+
+  selection_c = selectionSort(nums);
+  nums = disordered_nums;
+  insertion_c = insertionSort(nums);
+  nums = disordered_nums;
+  bubble_c = bubbleSort(nums);
+
+  outputFile << bubble_c << " " << selection_c << " " << insertion_c << "\n\n";
+
+  // Cantidad de números a buscar
+  inputFile >> q;
+  std::pair<int, int> result;
+
+  for (int i = 0; i < q; i++)
+  {
+    inputFile >> num;
+    result = sequentialSearch(nums, num);
+    outputFile << result.first << " " << result.second << " ";
+    result = binarySearch(nums, num);
+    outputFile << result.second << "\n";
+  }
+
+  inputFile.close();
+  outputFile.close();
+
+  return 0;
 }
--- tc1031-act2/search.h
+++ tc1031-act2/search.h
@@ -24,13 +24,20 @@
 //		   is not found in the vector.
 // =================================================================
 template <class T>
-int sequentialSearch(const std::vector<T> &v, T key) {
-	for (int i = 0; i < v.size(); i++) {
-		if (v[i] == key) {
-			return i;
-		}
-	}
-	return -1;
+std::pair<int, int> sequentialSearch(const std::vector<T> &v, T key)
+{
+  int count = 0;
+  for (int i = 0; i < v.size(); i++)
+  {
+    if (v[i] == key)
+    {
+      count++;
+      return std::pair<int, int>(i, count);
+    }
+    count++;
+  }
+  count++;
+  return std::pair<int, int>(-1, count);
 }
 
 // =================================================================
@@ -42,22 +49,33 @@ int sequentialSearch(const std::vector<T> &v, T key) {
 //		   is not found in the vector.
 // =================================================================
 template <class T>
-int binarySearch(const std::vector<T> &v, T key) {
-	int low, high, mid;
+std::pair<int, int> binarySearch(const std::vector<T> &v, T key)
+{
+  int low, high, mid;
+  int count = 0;
 
-	low = 0;
-	high = v.size() - 1;
-	while (low <= high) {
-		mid = low + ((high - low) / 2); // mid = (high + low) / 2;
-		if (key == v[mid]) {
-			return mid;
-		} else if (key < v[mid]) {
-			high = mid - 1;
-		} else {
-			low = mid + 1;
-		}
-	}
-	return -1;
+  low = 0;
+  high = v.size() - 1;
+  while (low <= high)
+  {
+    mid = low + ((high - low) / 2); // mid = (high + low) / 2;
+    if (key == v[mid])
+    {
+      count++;
+      return std::pair<int, int>(mid, count);
+    }
+    else if (key < v[mid])
+    {
+      count++;
+      high = mid - 1;
+    }
+    else
+    {
+      count++;
+      low = mid + 1;
+    }
+  }
+  return std::pair<int, int>(-1, count);
 }
 
 // =================================================================
@@ -71,21 +89,34 @@ int binarySearch(const std::vector<T> &v, T key) {
 //		   is not found in the vector.
 // =================================================================
 template <class T>
-int binaryRSearch(const std::vector<T> &v, int low, int high, T key) {
-	int mid;
+int binaryRSearch(const std::vector<T> &v, int low, int high, T key)
+{
+  int mid;
+  int count = 0;
 
-	if (low > high) {
-		return -1;
-	} else {
-		mid = low + ((high - low) / 2); // mid = (high + low) / 2;
-		if (key == v[mid]) {
-			return mid;
-		} else if (key < v[mid]) {
-			return binaryRSearch(v, low, mid - 1, key);
-		} else {
-			return binaryRSearch(v, mid + 1, high, key);
-		}
-	}
+  if (low > high)
+  {
+    count++;
+    return -1;
+  }
+  else
+  {
+    mid = low + ((high - low) / 2); // mid = (high + low) / 2;
+    if (key == v[mid])
+    {
+      count++;
+      return mid;
+    }
+    else if (key < v[mid])
+    {
+      return binaryRSearch(v, low, mid - 1, key);
+    }
+    else
+    {
+      count++;
+      return binaryRSearch(v, mid + 1, high, key);
+    }
+  }
 }
 
 #endif /* SEARCH_H */
